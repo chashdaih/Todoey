606481b4e869fc897bb8e8c794d6feb179f0ef0f
chas <chas@torus.mx>
2018-07-26 13:25:40 -0500

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by chas on 7/26/18.
+//  Copyright © 2018 chas. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
